48731d9fb7c733ae9daef7ca0e90ea69000ba6fa
unknown <bryankeltonadams@gmail.com>
2020-05-20 16:08:48 -0600

    Implemented stack

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,8 @@
 
 import sys
 
+
+
 class CPU:
     """Main CPU class."""
 
@@ -17,8 +19,14 @@ class CPU:
         self.branch_table = {
             0b10000010: self.ldi,
             0b01000111: self.prn,
-            0b10100010: self.mult
+            0b10100010: self.mult,
+            0b10100000: self.add,
+            0b01000110: self.pop,
+            0b01000101: self.push,
         }
+        self.interrupt_mask = 5
+        self.interrupt_status = 6
+        self.stack_pointer = 7
     def load(self, file_name):
 
         with open(file_name,'r') as fh:
@@ -29,6 +37,9 @@ class CPU:
             if processed_line != '#' and processed_line != '\n':
                 code.append(int(processed_line, 2))
         self.ram = code
+        print(self.ram)
+        for i, line in enumerate(code):
+            self.ram[i] = line
     def ram_read(self, MAR):
         MDR = self.ram[MAR]
         return MDR
@@ -65,6 +76,11 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+    def add(self):
+        registerA = self.ram[self.pc + 1]
+        registerB = self.ram[self.pc + 2]
+        self.alu("ADD", registerA, registerB)
+        self.pc += 2
     def ldi(self):
         register = self.ram[self.pc + 1]
         value = self.ram[self.pc + 2]
@@ -79,6 +95,18 @@ class CPU:
         registerB = self.ram[self.pc + 2]
         self.alu("MULT", registerA, registerB)
         self.pc += 2
+    def push(self):
+        register = self.ram[self.pc + 1]
+        val = self.reg[register]
+        self.stack_pointer -= 1
+        self.ram[self.stack_pointer] = val
+        self.pc += 1
+    def pop(self):
+        register = self.ram[self.pc + 1]
+        val = self.ram[self.stack_pointer]
+        self.reg[register] = val
+        self.stack_pointer += 1
+        self.pc += 1
     def run(self):
         """Run the CPU."""
         print('start cpu')
